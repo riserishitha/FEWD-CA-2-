7189f561aca338fcd926cd6c06c4a8358699da31
riserishitha <rishitha.n@kalvium.community>
2023-12-14 18:35:10 +0530

    commit changes

--- end.js
+++ end.js
@@ -6,5 +6,13 @@ let endsound = new Audio('./Explorer.mp3');
 endsound.play();
 endsound.loop = true;
 document.getElementById("name").innerHTML=localStorage.getItem("name")
+
+document.addEventListener("DOMContentLoaded", function() {
+const lostMessages = ["YOU LOST 😓!!!", "GAME OVER 😞!", "TRY AGAIN 😢!", "BETTER LUCK NEXT TIME 🍀"];
+const showScoreElement = document.getElementById("show-score");
+const randomLostMessage = lostMessages[Math.floor(Math.random() * lostMessages.length)];
+showScoreElement.innerHTML = randomLostMessage + " " + document.getElementById("name").innerHTML;
+});
+
  
 
